31d8b5e47afd8ad68be7c88a02af63cff7fbe928
Christian Schabetsberger <christian.schabetsberger@avocodo.com>
2023-07-26 20:43:38 +0200

    Sample for Meziantou.Analyzer

---
+++ Logging.Analyzer.Test/Controllers/LoggingController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Logging.Analyzer.Test.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoggingController : ControllerBase
+    {
+        private readonly ILogger<LoggingController> _logger;
+
+        public LoggingController(ILogger<LoggingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public void Get()
+        {
+            _logger.LogInformation("Test message: {Value}", Guid.NewGuid());
+            _logger.LogInformation("Test message: {Value}", string.Empty);
+
+            _logger.LogTestGuidWithoutGeneration(Guid.NewGuid());
+            _logger.LogTestStringWithoutGeneration(string.Empty);
+
+            _logger.LogTestGuid(Guid.NewGuid());
+            _logger.LogTestString(string.Empty);
+        }
+    }
+}
---
+++ Logging.Analyzer.Test/LoggingExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Logging.Analyzer.Test;
+
+public static partial class LoggingExtensions
+{
+    private static Action<ILogger, Guid, Exception?> _logTestGuidCallback =
+        LoggerMessage.Define<Guid>(
+            LogLevel.Information,
+            new EventId(1),
+            "Test message: {Value}");
+
+    public static void LogTestGuidWithoutGeneration(this ILogger logger, Guid value)
+        => _logTestGuidCallback(logger, value, default);
+
+    private static Action<ILogger, string, Exception?> _logTestStringCallback =
+        LoggerMessage.Define<string>(
+            LogLevel.Information,
+            new EventId(1),
+            "Test message: {Value}");
+
+    public static void LogTestStringWithoutGeneration(this ILogger logger, string value)
+        => _logTestStringCallback(logger, value, default);
+
+    [LoggerMessage(
+        Level = LogLevel.Information,
+        EventId = 1,
+        Message = "Test message: {Value}")]
+    public static partial void LogTestGuid(this ILogger logger, Guid value);
+
+    [LoggerMessage(
+        Level = LogLevel.Information,
+        EventId = 2,
+        Message = "Test message: {Value}")]
+    public static partial void LogTestString(this ILogger logger, string value);
+}
